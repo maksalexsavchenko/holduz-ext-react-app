c44f51b543c1913839e704c70fe817693c5b0929
maksalexsavchenko <makskidexom@gmail.com>
2022-05-03 00:58:50 +0300

    add files

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+function Home() {
+    return(
+        <div>
+            <h1>Welcome</h1>
+            <div className="test">Test</div>
+            <div>
+                <Link to="/legal-info" > legal </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
